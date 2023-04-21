11403cb5bbfde93317054ad3c1cc6247eb45b7b4
Conner Smith <conner@smith.net>
2023-04-21 12:07:06 -0500

    Update classifier.py
    This version of classifier is supposed to only output the JSON file, as needed by the web server

--- classifier.py
+++ classifier.py
@@ -1,7 +1,7 @@
-print("NutriMind food and nutrition classifier")
-print("Written with <3 by Conner Smith")
-print("Backend server-- Alpha Revision 6 - 4/11/23")
-print("Importing modules...")
+# NutriMind food and nutrition classifier
+# Written with <3 by Conner Smith
+# Backend server-- Alpha 8 - 4/21/23
+# This version is designed to strictly output the JSON data needed, without outputting anything else. 
 
 import requests
 import json
@@ -10,10 +10,12 @@ from PIL import Image, ImageOps  # Install pillow instead of PIL
 import numpy as np
 import os 
 import datetime
+import sys
+import subprocess
 
 timestamp = os.path.getmtime("keras_model.h5")
 last_modified = datetime.datetime.fromtimestamp(timestamp)
-print(f"Classifier model updated on: {last_modified}")
+# print(f"Classifier model updated on: {last_modified}")
 # Clear out the nutrition information file
 with open("nutrition.txt", "w") as f:
     f.write("")
@@ -23,13 +25,14 @@ np.set_printoptions(suppress=True)
 model = load_model("keras_model.h5", compile=False)
 # Load the labels
 class_names = open("labels.txt", "r").readlines()
-print("!!BEGIN CLASSIFICATION!!")
+# print("!!BEGIN CLASSIFICATION!!")
 # Create the array of the right shape to feed into the keras model
 # The 'length' or number of images you can put into the array is
 # determined by the first position in the shape tuple, in this case 1
 data = np.ndarray(shape=(1, 224, 224, 3), dtype=np.float32)
 # Replace this with the path to your image
-image_path = "IMG_6216.jpeg"
+arg4image = sys.argv[1]
+image_path = arg4image
 image = Image.open(image_path).convert("RGB")
 # resizing the image to be at least 224x224 and then cropping from the center
 size = (224, 224)
@@ -41,45 +44,45 @@ normalized_image_array = (image_array.astype(np.float32) / 127.5) - 1
 # Load the image into the array
 data[0] = normalized_image_array
 # Predicts the model
-prediction = model.predict(data)
+prediction = model.predict(data, verbose=0)
 index = np.argmax(prediction)
 class_name = class_names[index]
 confidence_score = prediction[0][index]
 # Pass the result to the nutrition API
 query = class_name[2:].strip()
 api_url = 'https://api.api-ninjas.com/v1/nutrition?query={}'.format(query)
-response = requests.get(api_url, headers={'X-Api-Key': 'YOUR-KEY-HERE'})
+response = requests.get(api_url, headers={'X-Api-Key': 'rJH5ilDDCLmiao2g41toaw==mykxvTT6Y9BusDo7'})
 if response.status_code == requests.codes.ok:
     # Write the classified food item to a text file
-    print(f"Class: {class_name[2:]}")
+    # print(f"Class: {class_name[2:]}")
     with open("result.txt", "w") as f:
         f.write(class_name[2:])
     conf = round(confidence_score * 100, 2)
-    print(f"Confidence Score: {conf}%")
+    # print(f"Confidence Score: {conf}%")
     response_data = response.json()[0]
     # Print nutrition information
     calories = response_data["calories"]
-    print("Calories:", calories)
+    # print("Calories:", calories)
     size = response_data["serving_size_g"]
-    print("Serving size:", size, "g")
+    # print("Serving size:", size, "g")
     satfat = response_data["fat_saturated_g"]
-    print("Saturated Fat:", satfat, "g")
+    # print("Saturated Fat:", satfat, "g")
     totalfat = response_data["fat_total_g"]
-    print("Total Fat:", totalfat, "g")
+    # print("Total Fat:", totalfat, "g")
     protein = response_data["protein_g"]
-    print("Protein:", protein, "g")
+    # print("Protein:", protein, "g")
     sodium = response_data["sodium_mg"]
-    print("Sodium:", sodium, "mg")
+    # print("Sodium:", sodium, "mg")
     potassium = response_data["potassium_mg"]
-    print("Potassium:", potassium, "mg")
+    # print("Potassium:", potassium, "mg")
     cholesterol = response_data["cholesterol_mg"]
-    print("Cholesterol:", cholesterol, "mg")
+    # print("Cholesterol:", cholesterol, "mg")
     carbs = response_data["carbohydrates_total_g"]
-    print("Total Carbohydrates:", carbs, "g")
+    # print("Total Carbohydrates:", carbs, "g")
     fiber = response_data["fiber_g"]
-    print("Fiber:", fiber, "g")
+    # print("Fiber:", fiber, "g")
     sugar = response_data["sugar_g"]
-    print("Sugar:", sugar, "g")
+    # print("Sugar:", sugar, "g")
     # Save the nutrition information to a file 
     with open(f"nutrition.txt", "a") as f:
         f.write(f"Calories: {str(calories)}\n")
@@ -93,6 +96,31 @@ if response.status_code == requests.codes.ok:
         f.write(f"Total Carbohydrates: {str(carbs)}g\n")
         f.write(f"Fiber: {str(fiber)}g\n")
         f.write(f"Sugar: {str(sugar)}g\n")
+    # Read in file1.txt
+    with open("result.txt", "r") as f:
+        name = f.readline().strip()
+        nutrition_info = {}
+        for line in f:
+            key, value = line.strip().split(': ')
+            nutrition_info[key] = value
+    # Read in file2.txt
+    with open("nutrition.txt", "r") as f:
+        nutrition_info2 = {}
+        for line in f:
+            key, value = line.strip().split(': ')
+            nutrition_info2[key] = value
+    # Combine the data from both files into one dictionary
+    jsresult = {"name": name, "nutrition_info": nutrition_info2}
+    # Output the result as a JSON object    
+    # json_dict = json.loads(jsresult)
+
+    # Convert the dictionary back to a JSON string with no backslashes or new lines
+    clean_json_str = json.dumps(jsresult, indent=None)
+
+    # Print the clean JSON string
+    print(clean_json_str)
+    with open(f"json.txt", "w") as f:
+        f.write(clean_json_str)
 else:
     # If the API encounters an error, log it to the file and display it in app
     print(f"Class: {class_name[2:]}")
